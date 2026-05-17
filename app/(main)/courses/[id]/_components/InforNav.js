66654186ce80cdbec1db9cf4a440b56d4cce1b49
Niaz-Tan <nuddintanim@gmail.com>
2026-05-17 21:01:32 +0600

    feat: working navigation added to course details page

--- app/(main)/courses/[id]/_components/InforNav.js
+++ app/(main)/courses/[id]/_components/InforNav.js
@@ -1,13 +1,54 @@
+"use client";
+import { useState } from "react";
+import Curriculum from "./Curriculum";
+import Instructor from "./Instructor";
+import Overview from "./Overview";
+
+const navData = [
+  { Name: "Overview" },
+  { Name: "Carriculum" },
+  { Name: "Instructor" },
+];
 const InforNav = () => {
+  const [currentNav, setCurrentNav] = useState("Overview");
+
+  const handleClick = (name) => {
+    setCurrentNav(name);
+  };
   return (
-    <div className="mt-8 flex">
-      <nav>
-        <ul className=" flex items-center gap-2 p-2 rounded-lg bg-zinc-900">
-          <li className="px-5 md:px-20 bg-zinc-950 py-1 rounded">Overview</li>
-          <li className="px-5 md:px-20 bg-zinc-950 py-1 rounded">Carriculum</li>
-          <li className="px-5 md:px-20 bg-zinc-950 py-1 rounded">Instructor</li>
+    <div className="mt-8">
+      <nav className="inline-block">
+        <ul className=" flex items-center gap-2 p-1 rounded-lg bg-zinc-900">
+          {navData.map((data, index) => {
+            const isActive = currentNav == data.Name;
+            return (
+              <li
+                key={index}
+                className={`px-5 md:px-20 py-2 rounded cursor-pointer
+                    ${isActive ? "bg-zinc-950" : ""}
+                    `}
+                onClick={() => handleClick(data.Name)}
+              >
+                {data.Name}
+              </li>
+            );
+          })}
         </ul>
       </nav>
+      {/* content */}
+      <div>
+       <div className={currentNav === "Overview" ? "block" : "hidden"}>
+          <Overview />
+        </div>
+
+        <div className={currentNav === "Carriculum" ? "block" : "hidden"}>
+          <Curriculum />
+        </div>
+
+        <div className={currentNav === "Instructor" ? "block" : "hidden"}>
+          <Instructor />
+        </div>
+      </div>
     </div>
   );
 };
