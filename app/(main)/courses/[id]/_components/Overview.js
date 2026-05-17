bd58927e64644481bbfffd57c93ce99fddebf546
Niaz-Tan <nuddintanim@gmail.com>
2026-05-17 21:56:42 +0600

    feat: overview component responsive now

--- app/(main)/courses/[id]/_components/Overview.js
+++ app/(main)/courses/[id]/_components/Overview.js
@@ -2,9 +2,14 @@ import { IoCheckmarkDoneOutline } from "react-icons/io5";
 
 const Overview = () => {
   return (
-    <div className="flex flex-col gap-6 mt-10">
-      <h1 className="font-bold text-4xl">Course Description</h1>
-      <p className="text-zinc-400 text-lg">
+    <div className="mt-10 flex flex-col gap-6">
+      {/* heading */}
+      <h1 className="text-3xl font-bold leading-tight sm:text-4xl">
+        Course Description
+      </h1>
+
+      {/* description */}
+      <p className="text-base leading-7 text-zinc-400 sm:text-lg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
         dignissimos ipsam. Neque quo delectus, fugiat illum velit aut error
         corporis assumenda veritatis ipsa eligendi amet quos magni sunt
@@ -12,24 +17,48 @@ const Overview = () => {
         consequuntur, deserunt aliquid. Omnis repudiandae optio impedit
         voluptatum dignissimos id hic fuga fugiat iste.
       </p>
-      <div className="flex flex-col gap-5 bg-zinc-900 rounded-lg p-15">
-        <h1 className="text-2xl font-semibold">What You Will Learn?</h1>
-        <ul className="grid grid-cols-2 text-zinc-300">
-          <li className="col-span-1 flex gap-2 items-center ">
-            <IoCheckmarkDoneOutline className="text-xl" /> Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Sequi, doloribus.
+
+      {/* learn section */}
+      <div className="flex flex-col gap-6 rounded-xl bg-zinc-900 p-5 sm:p-8 md:p-10">
+        <h1 className="text-xl font-semibold sm:text-2xl">
+          What You Will Learn?
+        </h1>
+
+        <ul className="grid grid-cols-1 gap-5 text-zinc-300 md:grid-cols-2">
+          <li className="flex items-start gap-3 leading-7">
+            <IoCheckmarkDoneOutline className="mt-1 shrink-0 text-xl text-green-500" />
+
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+              doloribus.
+            </span>
           </li>
-          <li className="col-span-1 flex gap-2 items-center">
-            <IoCheckmarkDoneOutline className="text-xl" /> Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Sequi, doloribus.
+
+          <li className="flex items-start gap-3 leading-7">
+            <IoCheckmarkDoneOutline className="mt-1 shrink-0 text-xl text-green-500" />
+
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+              doloribus.
+            </span>
           </li>
-          <li className="col-span-1 flex gap-2 items-center">
-            <IoCheckmarkDoneOutline className="text-xl" /> Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Sequi, doloribus.
+
+          <li className="flex items-start gap-3 leading-7">
+            <IoCheckmarkDoneOutline className="mt-1 shrink-0 text-xl text-green-500" />
+
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+              doloribus.
+            </span>
           </li>
-          <li className="col-span-1 flex gap-2 items-center">
-            <IoCheckmarkDoneOutline className="text-xl" /> Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Sequi, doloribus.
+
+          <li className="flex items-start gap-3 leading-7">
+            <IoCheckmarkDoneOutline className="mt-1 shrink-0 text-xl text-green-500" />
+
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+              doloribus.
+            </span>
           </li>
         </ul>
       </div>
